62bfe5b219f7f1e9b62ae400bb31e2e3c8dbb5d7
luccaz <luccazoppi@gmail.com>
2023-03-18 20:00:26 -0300

    feat: basic scoreboard implementation
    There's no clock function nor showing of the current score

--- GUI/scoreboard.py
+++ GUI/scoreboard.py
@@ -1,11 +1,25 @@
-from typing import Tuple
+from typing import AnyStr
+import pygame
+from constants import *
 
 class ScoreBoard:
     def __init__(self):
-        self.score = (0, 0)
-
-    def update(self, score: Tuple):
-        self.score = score
-
-    def draw(self):
-        pass
+        self.score = {"ally":0, "opponent":0}
+        self.frame = pygame.image.load("resources/scoreboard.png")
+        self.frame = pygame.transform.smoothscale_by(self.frame,SCOREBOARD_HEIGHT/self.frame.get_height())
+    def get_score(self):
+        return self.score
+    def update(self, character: AnyStr):
+        curr_score = self.score.get(character)
+        if curr_score == None:
+            raise ScoreUpdateError("Cannot register multiple goals at once")
+        self.score.update((character,curr_score + 1))
+    def draw(self, screen):
+        screen.blit(self.frame,((SCREEN_WIDTH-self.frame.get_width())/2,0))
+        return screen
+    
+class ScoreUpdateError(Exception):
+    def __init__(self, value) -> None:
+        self.value = value
+    def __str__(self) -> str:
+        return "Error: %s" % self.value
--- simulation.py
+++ simulation.py
@@ -3,17 +3,18 @@ import pygame
 from typing import Dict
 from GUI.player import Player
 from GUI.ball import Ball
+from GUI.scoreboard import ScoreBoard
 from constants import *
 from decision_making.manual_policy import ManualBehaviour
 
 class Simulation:
     def __init__(self) -> None:
-        self.score = (0, 0)
         self.ally = Player()
         self.opponent = Player()
         self.ball = Ball()
         self.ally_behaviour = ManualBehaviour()
-
+        self.scoreboard = ScoreBoard()
+        # self.clock = pygame.time.Clock()
     def update(self):
         ally_action = self.ally_behaviour.get_action(self.get_state())
         self.ally.update(ally_action)
@@ -24,6 +25,7 @@ class Simulation:
         screen = self.ally.draw(screen)
         screen = self.opponent.draw(screen)
         screen = self.ball.draw(screen)
+        screen = self.scoreboard.draw(screen)
         return screen
 
     def get_state(self) -> Dict:
@@ -32,5 +34,6 @@ class Simulation:
             "opponent_pose":self.opponent.get_pose(),
             "ball_pos":self.ball.pos,
             "ball_vel":self.ball.vel,
+            "score":self.scoreboard.get_score(),
         }
         
