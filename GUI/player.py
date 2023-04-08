6da0eae1455866db53f22cab038ad7d7f1a9c604
luccaz <luccazoppi@gmail.com>
2023-04-08 11:57:15 -0300

    fix: typo

--- GUI/player.py
+++ GUI/player.py
@@ -57,7 +57,7 @@ class Player(pygame.sprite.Sprite, GameElement):
         if self.__is_valid_update__(pose_updates):
             self._orientation, self._x, self._y = pose_updates
 
-    def __next_pose(self, action: Action) -> Tuple[float]:
+    def __next_pose__ (self, action: Action) -> Tuple[float]:
         self.vel = abs(self.speed * action.forward)
         if action.spin:
             self.velocity_orientation = (self._orientation - self.spin_speed * SAMPLE_TIME) % 360
