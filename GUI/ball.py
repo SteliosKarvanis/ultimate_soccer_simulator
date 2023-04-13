ab98538362ef4243e141a8ea2f7b0eddb31835aa
steliosKarvanis <stelioscbranco@hotmail.com>
2023-04-13 13:52:22 -0300

    improving ball colision to players

--- GUI/ball.py
+++ GUI/ball.py
@@ -1,63 +1,90 @@
 from typing import Tuple
 import pygame
-from pygame import Surface
 from utils.types import GameElement
 from GUI.field import LEFT_FRONT_GOAL_X, FIELD_LENGTH_Y, TOP_GOAL_Y
 from utils.configs import SAMPLE_TIME
 from GUI.player import Player
-from math import radians, cos, sin, degrees, sqrt, asin
+from math import radians, cos, sin, degrees, sqrt, acos, pi, tan
 
 BALL_RADIUS = 12
 FRICTION = 4
 BALL_DIAMETER = 2 * BALL_RADIUS
-EPSILON = 1
+TOLERANCE = 10
 
 
-def change_reference_frame(params: tuple, rf_params: tuple) -> tuple:
-    rf_x, rf_y, rf_theta, rf_v = rf_params
-    x0, y0, theta0, v0 = params
-    e_vx = v0 * cos(radians(theta0)) - rf_v * cos(radians(rf_theta))
-    e_vy = v0 * sin(radians(theta0)) - rf_v * sin(radians(rf_theta))
-    e_x = x0 - rf_x
-    e_y = y0 - rf_y
-    v = sqrt(e_vx**2 + e_vy**2)
-    if e_vx > 0.0 and v != 0:
-        theta = degrees(int(asin(e_vy / v)) - rf_theta) % 360
-    elif e_vx < 0.0 and v != 0:
-        theta = degrees(180 - int(asin(e_vy / v)) - rf_theta) % 360
+def rotate_pos(x0: float, y0: float, angle_rad: float) -> Tuple[float, float]:
+    x = x0 * cos(angle_rad) + y0 * sin(angle_rad)
+    y = -x0 * sin(angle_rad) + y0 * cos(angle_rad)
+    return x, y
+
+
+def cartesian_to_polar_vector(v_x: float, v_y: float) -> Tuple[float, float]:
+    v = sqrt(v_x**2 + v_y**2)
+    if v != 0:
+        theta_rad = acos(v_x / v)
+        if v_y < 0:
+            theta_rad = 2 * pi - theta_rad
     else:
-        if e_vy > 0.0:
-            theta = (90 - rf_theta) % 360
-        elif e_vy < 0.0:
-            theta = (270 - rf_theta) % 360
-        else:
-            theta = 0
-    x = e_x * cos(radians(rf_theta)) + e_y * sin(radians(rf_theta))
-    y = -e_x * sin(radians(rf_theta)) + e_y * cos(radians(rf_theta))
-    new_params = (x, y, theta, v)
+        theta_rad = 0
+    return v, theta_rad
+
+
+def polar_to_cartesian_vector(v: float, angle_rad: float) -> Tuple[float, float]:
+    return v * cos(angle_rad), v * sin(angle_rad)
+
+
+def translate_pos(x0: float, y0: float, delta_x: float, delta_y: float) -> Tuple[float, float]:
+    return x0 + delta_x, y0 + delta_y
+
+
+def global_to_element_referential(params: Tuple, rf_params: Tuple) -> Tuple[float, float]:
+    # Referential pose in world
+    rf_x, rf_y, rf_theta_deg, rf_v = rf_params
+    rf_theta_rad = radians(rf_theta_deg)
+    v_rf_x, v_rf_y = polar_to_cartesian_vector(rf_v, rf_theta_rad)
+
+    # Pose on global referential
+    x0, y0, theta0_deg, v0 = params
+    theta0_rad = radians(theta0_deg)
+    v0_x, v0_y = polar_to_cartesian_vector(v0, theta0_rad)
+
+    # Make relative positions (translation and rotation)
+    x0, y0 = translate_pos(x0, y0, -rf_x, -rf_y)
+    x, y = rotate_pos(x0, y0, rf_theta_rad)
+
+    # Make relative velocities (translation and rotation)
+    v0_x, v0_y = translate_pos(v0_x, v0_y, -v_rf_x, -v_rf_y)
+    v_x, v_y = rotate_pos(v0_x, v0_y, rf_theta_rad)
+
+    v, theta_rad = cartesian_to_polar_vector(v_x, v_y)
+    theta_deg = degrees(theta_rad)
+    new_params = (x, y, theta_deg, v)
     return new_params
 
 
-def back_to_original_frame(params: tuple, rf_params: tuple) -> tuple:
-    rf_x, rf_y, rf_theta, rf_v = rf_params
-    x, y, theta, v = params
-    x0 = x * cos(radians(rf_theta)) - y * sin(radians(rf_theta)) + rf_x
-    y0 = x * sin(radians(rf_theta)) + y * cos(radians(rf_theta)) + rf_y
-    vx0 = v * cos(radians(theta + rf_theta)) + rf_v * cos(radians(rf_theta))
-    vy0 = v * sin(radians(theta + rf_theta)) + rf_v * sin(radians(rf_theta))
-    v0 = sqrt(vx0**2 + vy0**2)
-    if vx0 > 0.0:
-        theta0 = (int(degrees(asin(vy0 / v0)))) % 360
-    elif vx0 < 0.0:
-        theta0 = (180 - int(degrees(asin(vy0 / v0)))) % 360
-    else:
-        if vy0 > 0.0:
-            theta0 = 90
-        elif vy0 < 0.0:
-            theta0 = 270
-        else:
-            theta0 = rf_theta + theta
-    return (x0, y0, theta0, v0)
+def element_ref_to_global_ref(params: tuple, rf_params: tuple) -> tuple:
+    # Referential pose in world
+    rf_x, rf_y, rf_theta_deg, rf_v = rf_params
+    rf_theta_rad = radians(rf_theta_deg)
+    rf_v_x, rf_v_y = polar_to_cartesian_vector(rf_v, rf_theta_rad)
+
+    # Pose on global referential
+    x0, y0, theta0_deg, v0 = params
+    theta0_rad = radians(theta0_deg)
+    v0_x, v0_y = polar_to_cartesian_vector(v0, theta0_rad)
+
+    # Make relative positions (translation and rotation)
+    x, y = rotate_pos(x0, y0, -rf_theta_rad)
+    x, y = translate_pos(x, y, rf_x, rf_y)
+
+    # Make relative velocities (translation and rotation)
+    v_x, v_y = rotate_pos(v0_x, v0_y, -rf_theta_rad)
+    v_x, v_y = translate_pos(v_x, v_y, rf_v_x, rf_v_y)
+
+    v, theta_rad = cartesian_to_polar_vector(v_x, v_y)
+    theta_deg = degrees(theta_rad)
+    new_params = (x, y, theta_deg, v)
+    return new_params
 
 
 class Ball(pygame.sprite.Sprite, GameElement):
@@ -123,17 +150,41 @@ class Ball(pygame.sprite.Sprite, GameElement):
 
     def collision_management(self, element: Player) -> bool:
         r_params = (element._x, element._y, int(element.velocity_orientation), element.vel)
-        x, y, orientation, v = change_reference_frame((self._x, self._y, self._orientation, self._vel), r_params)
         l1, l2 = element.size
-        if x - BALL_RADIUS <= l1 / 2 and x + BALL_RADIUS >= -l1 / 2 and y < l2 / 2 and y > -l2 / 2:
-            orientation = (180 - orientation) % 360
-            self._x, self._y, self._orientation, self._vel = back_to_original_frame((x, y, orientation, v), r_params)
-            return True
-
-        elif (
-            y - BALL_RADIUS <= l2 / 2 + EPSILON and y + BALL_RADIUS >= -l2 / 2 - EPSILON and x < l1 / 2 and x > -l1 / 2
-        ):
-            orientation = (-orientation) % 360
-            self._x, self._y, self._orientation, self._vel = back_to_original_frame((x, y, orientation, v), r_params)
-            return True
-        return False
+        collided = False
+        # Changing to the referential of the element
+        x, y, orientation, v = global_to_element_referential((self._x, self._y, self._orientation, self._vel), r_params)
+        v_x, v_y = polar_to_cartesian_vector(v, orientation)
+        # Check if has collision
+        if abs(x) < BALL_RADIUS + l2 / 2 and abs(y) < BALL_RADIUS + l2 / 2:
+            collided = True
+            # Check which side collided
+            y_top = l1 / 2 + BALL_RADIUS
+            y_bottom = -y_top
+            x_right = l2 / 2 + BALL_RADIUS
+            x_left = -x_right
+            # For each vertex, check it is above the line of movement of the ball, for example tr == True, check if the top right vertex is above the movement line of the ball
+            tr = (y_top - y + tan(orientation) * (x_right - x)) > 0
+            br = (y_bottom - y + tan(orientation) * (x_right - x)) > 0
+            tl = (y_top - y + tan(orientation) * (x_left - x)) > 0
+            bl = (y_bottom - y + tan(orientation) * (x_left - x)) > 0
+            # right side colision
+            if tr and not br and (orientation > 90 and orientation < 270):
+                x = BALL_RADIUS + l2 / 2 + TOLERANCE
+                v, orientation = cartesian_to_polar_vector(-abs(v_x), v_y)
+            # left side colision
+            elif tl and not bl and (orientation < 90 or orientation > 270):
+                x = -BALL_RADIUS - l2 / 2 - TOLERANCE
+                v, orientation = cartesian_to_polar_vector(-abs(v_x), v_y)
+            # top side colision
+            if tr and not tl and orientation > 180:
+                y = BALL_RADIUS + l1 / 2 + TOLERANCE
+                v, orientation = cartesian_to_polar_vector(v_x, -abs(v_y))
+            # bottom side colision
+            elif bl and not br and orientation < 180:
+                y = -BALL_RADIUS - l2 / 2 - TOLERANCE
+                v, orientation = cartesian_to_polar_vector(v_x, -abs(v_y))
+        # Back to the global referential
+        if collided:
+            self._x, self._y, self._orientation, self._vel = element_ref_to_global_ref((x, y, orientation, v), r_params)
+        return collided
